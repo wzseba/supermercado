240299877ce12e0b6e3b9bff005444b90012975d
wzseba <wzseba@gmail.com>
2023-09-24 22:36:05 -0300

    add: nueva clase con interface para ser reultilizada

---
+++ src/main/ProductoConDescuento.java
@@ -0,0 +1,24 @@
+package main;
+
+public class ProductoConDescuento extends Producto implements ConDescuento{
+
+	private double descuento;
+	
+	public ProductoConDescuento(String marca, double precio) {
+		super(marca,precio);
+		this.descuento = 0.0;
+	}
+	
+	/*sobreescritura de metodos interface*/
+	public void setDescuento(double des) {
+		this.descuento = des;
+	}
+	
+	public double getDescuento() {
+		return descuento;
+	}
+	
+	public double getPrecioDescuento() {
+		return this.getPrecio() - this.getPrecio() * this.getDescuento() / 100;
+	}
+}
