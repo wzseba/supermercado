60da490d8527b878367f9900ed92f81572507c1a
wzseba <wzseba@gmail.com>
2023-09-24 22:38:24 -0300

    add: se agrego get and set de atributos propios de la clase

--- src/main/Cereal.java
+++ src/main/Cereal.java
@@ -4,31 +4,41 @@ import java.time.LocalDate;
 
 public class Cereal extends Producto implements EsAlimento {
 	
-	private TipoCereal cereal;
+	private TipoCereal tipoCereal;
 	private LocalDate caducidad;
 	
-	public Cereal(String marca, double precio, TipoCereal cereal) {
+	public Cereal(String marca, double precio, TipoCereal tipoCereal) {
 		super(marca,precio);
-		this.cereal = cereal;
+		this.tipoCereal = tipoCereal;
 	}
 	
+	@Override
 	public void setCaducidad(LocalDate fc) {
 		this.caducidad = fc;
 	}
-	
+	@Override
 	public LocalDate getCaducidad() {
 		return caducidad;
 		
 	}
-	
+	@Override
 	public int getCalorias() {
-		return 0;
+		return this.tipoCereal.getCalorias();
 		
+	}
+	
+	/*get and set de atributos de la clase*/
+	public void setTipoCereal(TipoCereal tipoCereal) {
+		this.tipoCereal = tipoCereal;
+	}
+	
+	public TipoCereal getTipoCereal() {
+		return tipoCereal;
 	}
 
 	@Override
 	public String toString() {
-		return "Cereal [cereal=" + cereal + ", caducidad=" + caducidad + ", getCalorias()=" + getCalorias() + "]";
+		return "Cereal [cereal=" + tipoCereal + ", caducidad=" + caducidad + ", getCalorias()=" + getCalorias() + "]";
 	}
 	
 
